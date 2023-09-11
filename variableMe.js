78dd159ac89e82f0653d4a52ac7ef537a1a53fc3
terrencejihoonjung <terrencejung928@gmail.com>
2023-09-10 21:41:00 -0700

    learned about types and any

--- variableMe.js
+++ variableMe.js
@@ -1,2 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello Terrence";
+greetings.toLowerCase();
 console.log(greetings);
+// number
+var userId = 334455;
+// boolean
+var isLoggedIn = false;
+// good practice
+var userId2 = 334455;
--- variableMe.ts
+++ variableMe.ts
@@ -2,4 +2,22 @@ let greetings: string = "Hello Terrence";
 greetings.toLowerCase();
 console.log(greetings);
 
+// number
+let userId: number = 334455;
+
+// boolean
+let isLoggedIn: boolean = false;
+
+// good practice
+let userId2 = 334455;
+
+// avoid using any
+let hero; // given type "any" by default
+
+function getHero() {
+  return "thor";
+}
+
+hero = getHero(); // notice this variable is initially having a type of "any"
+
 export {};
