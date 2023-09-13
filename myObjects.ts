5ed4cce1f162bfc50549df849f70b326256314b4
terrencejihoonjung <terrencejung928@gmail.com>
2023-09-12 23:04:46 -0700

    type aliases

--- myObjects.ts
+++ myObjects.ts
@@ -4,17 +4,32 @@ const User = {
   isActive: true,
 };
 
-// Object as parameter
-function createUser({ name: string, isPaid: boolean }) {}
+// // Object as parameter
+// function createUser({ name: string, isPaid: boolean }) {}
 
-let newUser = { name: "terrence", isPaid: false, email: "t@gmail.com" };
+// let newUser = { name: "terrence", isPaid: false, email: "t@gmail.com" };
 
-// Weird behavior where object argument has more information than it should have, but function accepts it
-createUser(newUser);
+// // Weird behavior where object argument has more information than it should have, but function accepts it
+// createUser(newUser);
 
 // Object return type
 function createCourse(): { name: string; price: number } {
   return { name: "reactjs", price: 399 };
 }
+
+// Type Alias
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+
+// Returning object of type "User"
+function createUser(user: User): User {
+  return { name: "", email: "", isActive: true };
+}
+
+// Function call with type User argument
+createUser({ name: "", email: "", isActive: true });
 
 export {};
