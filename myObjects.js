7ac643032167691ab3476e80f173da65b0f22bdc
terrencejihoonjung <terrencejung928@gmail.com>
2023-09-13 23:23:05 -0700

    readonly and ?

---
+++ myObjects.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var User = {
+    name: "terrence",
+    email: "terrence@gmail.com",
+    isActive: true,
+};
+// User instantiation
+var myUser = {
+    _id: "1245",
+    name: "terrence",
+    email: "terrence@gmail.com",
+    isActive: true,
+};
+// Setting data
+myUser.email = "jung@gmail.com";
--- myObjects.ts
+++ myObjects.ts
@@ -13,23 +13,54 @@ const User = {
 // createUser(newUser);
 
 // Object return type
-function createCourse(): { name: string; price: number } {
-  return { name: "reactjs", price: 399 };
-}
+// function createCourse(): { name: string; price: number } {
+//   return { name: "reactjs", price: 399 };
+// }
 
-// Type Alias
+// // Type Alias
+// type User = {
+//   name: string;
+//   email: string;
+//   isActive: boolean;
+// };
+
+// // Returning object of type "User"
+// function createUser(user: User): User {
+//   return { name: "", email: "", isActive: true };
+// }
+
+// // Function call with type User argument
+// createUser({ name: "", email: "", isActive: true });
+
+// readonly keyword + optional marker
 type User = {
+  readonly _id: string;
   name: string;
   email: string;
   isActive: boolean;
+  creditCardDetails?: number;
 };
 
-// Returning object of type "User"
-function createUser(user: User): User {
-  return { name: "", email: "", isActive: true };
-}
+// User instantiation
+let myUser: User = {
+  _id: "1245",
+  name: "terrence",
+  email: "terrence@gmail.com",
+  isActive: true,
+};
 
-// Function call with type User argument
-createUser({ name: "", email: "", isActive: true });
+type cardNumber = {
+  cardNumber: string;
+};
+
+type cardDate = {
+  cardDate: string;
+};
+
+// Mix and Match Type Aliases
+type cardDetails = cardNumber &
+  cardDate & {
+    cvv: number;
+  };
 
 export {};
